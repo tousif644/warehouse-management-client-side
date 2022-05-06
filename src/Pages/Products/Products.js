2f9df1c04fecdd7d5a671e203c60a6503d3e889d
tousif644 <tousif2567@gmail.com>
2022-05-06 07:07:17 +0600

    fetched products from localhost

---
+++ src/Pages/Products/Products.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import "./Products.css";
+const Products = () => {
+  //state
+  const [products, setProducts] = useState([]);
+
+  //hook
+  useEffect(() => {
+    const url = `http://localhost:5000/products`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+  }, []);
+
+  const p = products.slice(0,6);
+  return (
+    <div>
+      <div className="container-fluid" id="products">
+        <h1 className="text-center my-3">Featured Products</h1>
+        <div className="container">
+          <div className="row row-cols-lg-3">
+            {p.map((product) => (
+              <Product key={product._id} products={product}></Product>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
